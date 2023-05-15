785927a1da9305725a30e33035e8e7c0cb940473
chirag-agrawal08x <chirag@azodha.com>
2023-05-16 03:11:13 +0530

    minor fixes in training model

--- models/training.js
+++ models/training.js
@@ -1,34 +1,30 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const trainingSchema = mongoose.Schema({
-    training_id : {
-        type: String,
-        required : false
-    },
-    name : {
-        type : String,
-        required : true
-    },
-    summary : {
-        type : String,
-        required: true
-    },
-    instructor : {
-        type : String,
-        required : true
-    },
-    duration : {
-        type: String,
-        required : false,
-    },
-    thumbnail : {
-        type : String,
-        required : false
-    },
-    src : {
-        type: String,
-        required : true
-    }
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  summary: {
+    type: String,
+    required: true,
+  },
+  instructor: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: String,
+    required: false,
+  },
+  thumbnail: {
+    type: String,
+    required: false,
+  },
+  src: {
+    type: String,
+    required: true,
+  },
+});
 
-module.exports = mongoose.model('Trainings', trainingSchema);
+module.exports = mongoose.model("Trainings", trainingSchema);
